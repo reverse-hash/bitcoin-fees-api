e94dba95a6624fa669e667ee6fa9343da4bcf6ff
reverse-hash <11018433+reverse-hash@users.noreply.github.com>
2024-01-20 21:09:55 +0100

    Add support for SSL certificates

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import 'dotenv/config';
 import express from 'express';
+import fs from 'fs';
+import https from 'https';
 import estimatedFees from './estimated-fees.js';
 import { getStatus } from './health.js';
 
+var privateKey = fs.readFileSync('./ssl/privatekey.pem');
+var certificate = fs.readFileSync('./ssl/certificate.pem');
+
 const app = express();
+
+https
+  .createServer(
+    {
+      key: privateKey,
+      cert: certificate,
+    },
+    app,
+  )
+  .listen(process.env.PORT);
 
 app.get('/estimated-fees', (_, res) => {
   res.send(estimatedFees());
@@ -11,8 +26,4 @@ app.get('/estimated-fees', (_, res) => {
 
 app.get('/health', (_, res) => {
   return res.json({ status: getStatus() });
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
 });
