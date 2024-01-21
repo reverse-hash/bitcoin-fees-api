84f69beeb41166d84e1c264793aff6f6554b3d1c
reverse-hash <11018433+reverse-hash@users.noreply.github.com>
2024-01-21 19:12:08 +0100

    Reorg app.js

--- src/app.js
+++ src/app.js
@@ -7,22 +7,14 @@ import https from 'https';
 import estimatedFees from './estimated-fees.js';
 import { health } from './health.js';
 
-var key = fs.readFileSync('./ssl/domain.key'); // Domain private key
-var cert = fs.readFileSync('./ssl/domain.crt'); // Domain certificate
-var ca = fs.readFileSync('./ssl/ca.crt'); // Authority certificate
+const key = fs.readFileSync('./ssl/domain.key'); // Domain private key
+const cert = fs.readFileSync('./ssl/domain.crt'); // Domain certificate
+const ca = fs.readFileSync('./ssl/ca.crt'); // Authority certificate
 
 const app = express();
+app.use(cors());
+app.get('/estimated-fees', (_, res) => res.send(estimatedFees()));
+app.get('/health', (_, res) => res.json(health()));
 
 const options = { key, cert, ca };
-
-app.use(cors());
-
-app.get('/estimated-fees', (_, res) => {
-  res.send(estimatedFees());
-});
-
-app.get('/health', (_, res) => {
-  return res.json(health());
-});
-
 https.createServer(options, app).listen(process.env.PORT);
